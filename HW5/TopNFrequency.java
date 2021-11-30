dc9be694da6f1850770ee159744e16217516bb43
gih6 <54678622+gih6@users.noreply.github.com>
2021-11-29 20:54:13 -0500

    Update TopNFrequency.java

--- HW5/TopNFrequency.java
+++ HW5/TopNFrequency.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -16,6 +18,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+//Some Inspiration from: https://www.geeksforgeeks.org/how-to-find-top-n-records-using-mapreduce/
 public class TopNFrequencies {
     //key is the TERM value is the Count & Document it is from!!
     public static class TokenizerMapper
@@ -29,18 +32,41 @@ public class TopNFrequencies {
             StringTokenizer itr = new StringTokenizer(value.toString().toLowerCase().replaceAll("\\d","").replaceAll("[^a-zA-Z ]",""));
             while (itr.hasMoreTokens()) {
                 String wordDoc = itr.nextToken();
-                if(count.containsKey(wordDoc)){
+                //stop list
+                if(wordDoc.equals("if") ||
+                        wordDoc.equals("he")||
+                        wordDoc.equals("she")||
+                        wordDoc.equals("and")||
+                        wordDoc.equals("can")||
+                        wordDoc.equals("of")||
+                        wordDoc.equals("a") ||
+                        wordDoc.equals("an") ||
+                        wordDoc.equals("to") ||
+                        wordDoc.equals("the") ||
+                        wordDoc.equals("in") ||
+                        wordDoc.equals("is") ||
+                        wordDoc.equals("it") ||
+                        wordDoc.equals("from") ||
+                        wordDoc.equals("for") ||
+                        wordDoc.equals("not") ||
+                        wordDoc.equals("this") ||
+                        wordDoc.equals("i")
+                ) {
+                    continue;
+                }
+                else if(count.containsKey(wordDoc)){
                     count.put(wordDoc,count.get(wordDoc)+1); // adding to existing on
                 }else {
                     count.put(wordDoc, 1); //first instance of word in file
                 }
             }
-
+            //now take top 5 from this list
             for (Map.Entry<String, Integer> entry : count.entrySet()) {
                 word.set(entry.getKey());
                 IntWritable test = new IntWritable(entry.getValue());
                 context.write(word, test);
             }
+            //now take top 5 from this list
         }
     }
     public static class KeyComparator extends WritableComparator {
@@ -59,18 +85,39 @@ public class TopNFrequencies {
     public static class IntSumReducer
             extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
+        TreeMap<Integer,String> top5Words = new TreeMap<Integer,String>();
 
         public void reduce(Text key, Iterable<IntWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
+
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
-            result.set(sum);
-            context.write(key, result);
+
+            //putting in ALL sum and key to the Tree map
+            //write out the top 5 frequency
+            top5Words.put(sum,key.toString());
+            if(top5Words.size() > 5){
+                top5Words.remove(top5Words.firstKey()); // remove the smallest key from the tree
+            }
+
+
         }
+        //this actually sends out the top 5 data
+        public void cleanup (Context context) throws IOException, InterruptedException
+        {
+
+            for(Map.Entry<Integer,String> entry: top5Words.entrySet()){
+                IntWritable count = new IntWritable(entry.getKey());
+                Text word = new Text(entry.getValue());
+                context.write(word,count);
+            }
+        }
+
     }
+
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
@@ -85,8 +132,6 @@ public class TopNFrequencies {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
     }
 }
-
-//Notes on the Inverting Indexes:
-//want  to sort by BOTH FOLDER and FILE NAME and by WORD!! -- need to modify this as such
